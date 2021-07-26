0a035293efc6f2d2f004855fb490b40e0a984bd8
Blake Nuttall <blakedyblake@gmail.com>
2021-07-26 15:53:22 -0600

    +Extra credit/ further study

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+const { ENGINE_METHOD_ALL } = require('constants');
 const readline = require('readline');
 
 const reader = readline.createInterface({
@@ -5,6 +6,12 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
+//Comment instructions for the user
+console.log("+ to add, - to subtract, x to multiply, / to divide, sqrt to take a square root.");
+console.log("Each takes 2 arguments, except for sqrt only the first one counts ;)");
+
+console.log("rem for remainder in a division and pwr for exponential equations");
+console.log("pwr 3 2 would be three squared");
 
 
 reader.question("What would you like to calculate?", function(input){
@@ -14,8 +21,34 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	
 
 	// This line closes the connection to the command line interface.
+	if(mathSymbol === '+'){
+		console.log(`${num1} + ${num2} = ${num1 + num2}`);
+	}
+	else if(mathSymbol === '-'){
+		console.log(`${num1} - ${num2} = ${num1 - num2}`);
+	}
+	else if((mathSymbol === 'x') || (mathSymbol === 'X') ||(mathSymbol === '*')){
+		console.log(`${num1} x ${num2} = ${num1 * num2}`);
+	}
+	else if((mathSymbol === "/")) {
+		console.log(`${num1} / ${num2} = ${num1 / num2}`);
+	}
+	else if(mathSymbol === "sqrt"){
+		console.log(`The square root of ${num1} is ${Math.sqrt(num1)}`);
+	}
+	else if(mathSymbol === "pwr"){
+		console.log(`${num1} to the ${num2} power is ${Math.pow(num1, num2)}`)
+
+	}
+	else if(mathSymbol === "rem"){
+		console.log(`The remainer of ${num1} divided by ${num2} is ${num1 % num2}`);
+	}
+	else{
+		console.log("Invalid response");
+	}
 	reader.close()
 
 });
